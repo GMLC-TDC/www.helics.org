e25d9d25432201eb3be70eb390792d243d7a756f
Dheepak Krishnamurthy <me@kdheepak.com>
2019-10-27 20:01:32 -0600

    Initial commit

---
+++ components/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="helics-logo">
+    <img src="../assets/logo.png"
+    height="100px"
+    width="100px"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+img {
+  max-width:120px;
+  max-height:100px;
+  width: auto;
+  height: auto;
+}
+</style>
