ca7d59512ce47447cec6229cf8a062abab09c453
Dheepak Krishnamurthy <me@kdheepak.com>
2021-08-14 15:07:17 -0600

    Run yarn upgrade

--- pages/badges.vue
+++ pages/badges.vue
@@ -1,32 +1,44 @@
 <template>
-  <b-container style="padding-top: 60px;">
+  <b-container style="padding-top: 60px">
     <b-row>
       <b-col class="mt-3 ml-0 mr-0" cols="12">
         <b-row class="mt-2 mb-0 ml-0 mr-0">
           <h3>Badges</h3>
         </b-row>
-        <b-row class="mt-2 mb-0 ml-0 mr-0">
-        </b-row>
+        <b-row class="mt-2 mb-0 ml-0 mr-0"> </b-row>
         <b-row class="mt-2 mb-4 ml-0 mr-0">
           <b-col class="ml-0 mr-0">
             <b-row class="ml-0">
               <p>
-                &raquo;
-                <a
-                  href="https://github.com/GMLC-TDC/HELICS/releases/tag/v2.6.1"
-                >
-                <img alt="GitHub all releases" src="https://img.shields.io/github/downloads/GMLC-TDC/helics/total">
-                </a>
-
-<img alt="GitHub issues" src="https://img.shields.io/github/issues-raw/GMLC-TDC/HELICS">
-
-<img alt="GitHub closed issues" src="https://img.shields.io/github/issues-closed-raw/GMLC-TDC/HELICS">
-
-<img alt="GitHub pull requests" src="https://img.shields.io/github/issues-pr-raw/GMLC-TDC/HELICS">
-
-<img alt="GitHub closed pull requests" src="https://img.shields.io/github/issues-pr-closed-raw/GMLC-TDC/HELICS">
-
-<img alt="GitHub contributors" src="https://img.shields.io/github/contributors-anon/GMLC-TDC/HELICS">
+                <img
+                  alt="GitHub all releases"
+                  src="https://img.shields.io/github/downloads/GMLC-TDC/helics/total"
+                />
+                <br />&raquo;
+                <img
+                  alt="GitHub issues"
+                  src="https://img.shields.io/github/issues-raw/GMLC-TDC/HELICS"
+                />
+                <br />&raquo;
+                <img
+                  alt="GitHub closed issues"
+                  src="https://img.shields.io/github/issues-closed-raw/GMLC-TDC/HELICS"
+                />
+                <br />&raquo;
+                <img
+                  alt="GitHub pull requests"
+                  src="https://img.shields.io/github/issues-pr-raw/GMLC-TDC/HELICS"
+                />
+                <br />&raquo;
+                <img
+                  alt="GitHub closed pull requests"
+                  src="https://img.shields.io/github/issues-pr-closed-raw/GMLC-TDC/HELICS"
+                />
+                <br />&raquo;
+                <img
+                  alt="GitHub contributors"
+                  src="https://img.shields.io/github/contributors-anon/GMLC-TDC/HELICS"
+                />
                 <br />&raquo;
               </p>
             </b-row>
@@ -37,8 +49,7 @@
   </b-container>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style>
 img {
