1880df3034adae28df020070bda738b2d5133a8f
kdheepak <kdheepak@users.noreply.github.com>
2021-08-14 22:26:53 +0000

    deploy: 2a32fe3882962a5c2cb631c453668567dbd547c2

--- _nuxt/static/1628979768/badges/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhelics.org\u002F_nuxt\u002Fstatic\u002F1628979768",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbadges",config:{_app:{basePath:"\u002Fhelics.org\u002F",assetsPath:"\u002Fhelics.org\u002F_nuxt\u002F",cdnURL:a}}}}(null));
