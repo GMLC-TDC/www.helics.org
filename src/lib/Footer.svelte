d0665514fad0e03d2ece0146b72fe6b1a60b67ae
Dheepak Krishnamurthy <me@kdheepak.com>
2022-01-06 11:49:42 -0700

    chore: Update files ♻️

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="bg-gray-900 text-gray-200 shadow-lg text-center lg:text-left mt-6">
   <div class="text-gray-200 text-center p-4" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2021 Copyright:
+    © 2022 Copyright:
     <a class="text-gray-100" href="https://github.com/GMLC-TDC/HELICS">HELICS</a>
   </div>
 </footer>
