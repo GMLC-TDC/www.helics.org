f202444f400f573ebe53fc8fe2612d020248636a
Dheepak Krishnamurthy <me@kdheepak.com>
2022-01-06 01:46:49 -0700

    Add title

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,19 @@
   import '../app.css'
   import NavBar from '$lib/NavBar.svelte'
   import Footer from '$lib/Footer.svelte'
+  import { page } from '$app/stores'
+  function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  let title = capitalize(page.url.pathname.substring(1))
 </script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="title" content={title} />
+  <meta property="og:title" content={title} />
+  <meta property="twitter:title" content={title} />
+</svelte:head>
 
 <div class="h-screen w-full grid grid-rows-[auto_1fr_auto]">
   <NavBar />
