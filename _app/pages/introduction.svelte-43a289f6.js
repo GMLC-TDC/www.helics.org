797ed2cf810af0a6645cc66106f1ed3bae18c4f4
kdheepak <kdheepak@users.noreply.github.com>
2022-01-06 07:36:20 +0000

    deploy: cb665d252a47601b4ef7dba4d20ae388eadb2754

---
+++ _app/pages/introduction.svelte-43a289f6.js
@@ -0,0 +1,93 @@
+import{S as no,i as ro,s as lo,e as o,t as e,j as d,c as n,a as l,g as a,d as r,l as u,b as h,f as io,G as t,K as Ia}from"../chunks/vendor-51f8c06b.js";function so(oo){let I,b,A,et,Ct,bt,at,_t,It,_,L,ot,Lt,St,nt,yt,At,g,Ht,B,Tt,Bt,Dt,kt,D,Rt,wt,xt,Pt,k,Gt,Mt,Vt,Wt,R,Jt,Ot,S,rt,qt,Ut,lt,jt,Nt,m,zt,w,Kt,Xt,Yt,Ft,x,Qt,Zt,$t,te,P,ee,ae,oe,ne,G,re,le,y,it,ie,se,T,he,M,ce,de,ue,p,fe,pe,V,ge,me,ve,Ee,W,Ce,be,_e,Ie,J,Le,Se,ye,Ae,O,He,Te,H,i,st,Be,De,ht,ke,Re,q,we,xe,Pe,Ge,ct,Me,Ve,U,We,Je,Oe,qe,dt,Ue,je,j,Ne,ze,Ke,Xe,ut,Ye,Fe,N,Qe,Ze,$e,s,ft,ta,ea,z,aa,oa,na,ra,la,ia,K,sa,ha,ca,da,ua,fa,X,pa,ga,ma,va,Ea,Ca,Y,ba,_a;return{c(){I=o("article"),b=o("div"),A=o("div"),et=o("h3"),Ct=e("Getting Started"),bt=d(),at=o("p"),_t=e(`The following is useful information to getting started with the HELICS co-simulation
+        library.`),It=d(),_=o("div"),L=o("div"),ot=o("h4"),Lt=e("Introduction"),St=d(),nt=o("p"),yt=e(`Basic introduction to HELICS language, installation instructions and anything else you
+          need to get started.`),At=d(),g=o("p"),Ht=e(`\xBB
+          `),B=o("a"),Tt=e("Installation"),Bt=d(),Dt=o("br"),kt=e(`
+          \xBB
+          `),D=o("a"),Rt=e("Terminology"),wt=d(),xt=o("br"),Pt=e(`
+          \xBB
+          `),k=o("a"),Gt=e("Base Example Co-Simulation"),Mt=d(),Vt=o("br"),Wt=e(`
+          \xBB
+          `),R=o("a"),Jt=e("Try HELICS online"),Ot=d(),S=o("div"),rt=o("h4"),qt=e("User Guide"),Ut=d(),lt=o("p"),jt=e(`A user guide for HELICS. This assumes a familiarity with general programming concepts, and
+          some familiarity with domain tools.`),Nt=d(),m=o("p"),zt=e(`\xBB
+          `),w=o("a"),Kt=e("Co-simulation Overview"),Xt=d(),Yt=o("br"),Ft=e(`
+          \xBB
+          `),x=o("a"),Qt=e("Key Concepts"),Zt=d(),$t=o("br"),te=e(`
+          \xBB
+          `),P=o("a"),ee=e("Co-Simulation Stages"),ae=d(),oe=o("br"),ne=e(`
+          \xBB
+          `),G=o("a"),re=e("Co-Simulation Timing Configuration"),le=d(),y=o("div"),it=o("h4"),ie=e("API"),se=d(),T=o("p"),he=e(`Configuration options documentation for the
+          `),M=o("a"),ce=e("HELICS Library"),de=e("."),ue=d(),p=o("p"),fe=o("br"),pe=e(`\xBB
+          `),V=o("a"),ge=e("C API Documentation"),me=d(),ve=o("br"),Ee=e(`\xBB
+          `),W=o("a"),Ce=e("Python API Documentation"),be=d(),_e=o("br"),Ie=e(`\xBB
+          `),J=o("a"),Le=e("Julia API Documentation"),Se=d(),ye=o("br"),Ae=e(`\xBB
+          `),O=o("a"),He=e("C++ Doxygen Documentation"),Te=d(),H=o("div"),i=o("div"),st=o("h4"),Be=e("Want to know more?"),De=e(`
+        \xBB
+        `),ht=o("strong"),ke=e("Looking for tutorials/code snippets?"),Re=e(` Check out our
+        `),q=o("a"),we=e("HELICS Examples Repository"),xe=e(`.
+        `),Pe=o("br"),Ge=e(`\xBB
+        `),ct=o("strong"),Me=e("Looking for videos?"),Ve=e(` Check out
+        `),U=o("a"),We=e("HELICS Tutorials playlist"),Je=e(`
+        section.
+        `),Oe=o("br"),qe=e(`\xBB
+        `),dt=o("strong"),Ue=e("Want to ask a question?"),je=e(` Come find us on
+        `),j=o("a"),Ne=e("gitter and get in touch"),ze=e(`.
+        `),Ke=o("br"),Xe=e(`\xBB
+        `),ut=o("strong"),Ye=e("Have a feature request or bug report? Want to make a contribution or provide feedback?"),Fe=e(`
+        All development is open source and publically available, check out the sources over at
+        `),N=o("a"),Qe=e("our GitHub organization"),Ze=e("."),$e=d(),s=o("div"),ft=o("h4"),ta=e("Additional Information"),ea=e(`
+        \xBB
+        `),z=o("a"),aa=e("Use Conda to install HELICS for Python"),oa=e(`.
+        `),na=o("br"),ra=d(),la=o("br"),ia=e(`\xBB
+        `),K=o("a"),sa=e(`Check out our Windows installers for C/C++ Shared Libraries with Python, MATLAB, and Java
+          language bindings`),ha=e(`.
+        `),ca=o("br"),da=d(),ua=o("br"),fa=e(`\xBB
+        `),X=o("a"),pa=e(`Download precompiled C/C++ Shared Libraries for Windows, Mac, Linux platforms to link in
+          your application`),ga=e(`.
+        `),ma=o("br"),va=d(),Ea=o("br"),Ca=e(`\xBB
+        `),Y=o("a"),ba=e("Try HELICS in Julia"),_a=e("."),this.h()},l(pt){I=n(pt,"ARTICLE",{class:!0});var gt=l(I);b=n(gt,"DIV",{class:!0});var F=l(b);A=n(F,"DIV",{class:!0});var mt=l(A);et=n(mt,"H3",{});var La=l(et);Ct=a(La,"Getting Started"),La.forEach(r),bt=u(mt),at=n(mt,"P",{});var Sa=l(at);_t=a(Sa,`The following is useful information to getting started with the HELICS co-simulation
+        library.`),Sa.forEach(r),mt.forEach(r),It=u(F),_=n(F,"DIV",{class:!0});var Q=l(_);L=n(Q,"DIV",{});var Z=l(L);ot=n(Z,"H4",{});var ya=l(ot);Lt=a(ya,"Introduction"),ya.forEach(r),St=u(Z),nt=n(Z,"P",{});var Aa=l(nt);yt=a(Aa,`Basic introduction to HELICS language, installation instructions and anything else you
+          need to get started.`),Aa.forEach(r),At=u(Z),g=n(Z,"P",{});var E=l(g);Ht=a(E,`\xBB
+          `),B=n(E,"A",{href:!0});var Ha=l(B);Tt=a(Ha,"Installation"),Ha.forEach(r),Bt=u(E),Dt=n(E,"BR",{}),kt=a(E,`
+          \xBB
+          `),D=n(E,"A",{href:!0});var Ta=l(D);Rt=a(Ta,"Terminology"),Ta.forEach(r),wt=u(E),xt=n(E,"BR",{}),Pt=a(E,`
+          \xBB
+          `),k=n(E,"A",{href:!0});var Ba=l(k);Gt=a(Ba,"Base Example Co-Simulation"),Ba.forEach(r),Mt=u(E),Vt=n(E,"BR",{}),Wt=a(E,`
+          \xBB
+          `),R=n(E,"A",{href:!0});var Da=l(R);Jt=a(Da,"Try HELICS online"),Da.forEach(r),E.forEach(r),Z.forEach(r),Ot=u(Q),S=n(Q,"DIV",{});var $=l(S);rt=n($,"H4",{});var ka=l(rt);qt=a(ka,"User Guide"),ka.forEach(r),Ut=u($),lt=n($,"P",{});var Ra=l(lt);jt=a(Ra,`A user guide for HELICS. This assumes a familiarity with general programming concepts, and
+          some familiarity with domain tools.`),Ra.forEach(r),Nt=u($),m=n($,"P",{});var C=l(m);zt=a(C,`\xBB
+          `),w=n(C,"A",{href:!0});var wa=l(w);Kt=a(wa,"Co-simulation Overview"),wa.forEach(r),Xt=u(C),Yt=n(C,"BR",{}),Ft=a(C,`
+          \xBB
+          `),x=n(C,"A",{href:!0});var xa=l(x);Qt=a(xa,"Key Concepts"),xa.forEach(r),Zt=u(C),$t=n(C,"BR",{}),te=a(C,`
+          \xBB
+          `),P=n(C,"A",{href:!0});var Pa=l(P);ee=a(Pa,"Co-Simulation Stages"),Pa.forEach(r),ae=u(C),oe=n(C,"BR",{}),ne=a(C,`
+          \xBB
+          `),G=n(C,"A",{href:!0});var Ga=l(G);re=a(Ga,"Co-Simulation Timing Configuration"),Ga.forEach(r),C.forEach(r),$.forEach(r),le=u(Q),y=n(Q,"DIV",{});var tt=l(y);it=n(tt,"H4",{});var Ma=l(it);ie=a(Ma,"API"),Ma.forEach(r),se=u(tt),T=n(tt,"P",{});var vt=l(T);he=a(vt,`Configuration options documentation for the
+          `),M=n(vt,"A",{href:!0});var Va=l(M);ce=a(Va,"HELICS Library"),Va.forEach(r),de=a(vt,"."),vt.forEach(r),ue=u(tt),p=n(tt,"P",{});var v=l(p);fe=n(v,"BR",{}),pe=a(v,`\xBB
+          `),V=n(v,"A",{href:!0});var Wa=l(V);ge=a(Wa,"C API Documentation"),Wa.forEach(r),me=u(v),ve=n(v,"BR",{}),Ee=a(v,`\xBB
+          `),W=n(v,"A",{href:!0});var Ja=l(W);Ce=a(Ja,"Python API Documentation"),Ja.forEach(r),be=u(v),_e=n(v,"BR",{}),Ie=a(v,`\xBB
+          `),J=n(v,"A",{href:!0});var Oa=l(J);Le=a(Oa,"Julia API Documentation"),Oa.forEach(r),Se=u(v),ye=n(v,"BR",{}),Ae=a(v,`\xBB
+          `),O=n(v,"A",{href:!0});var qa=l(O);He=a(qa,"C++ Doxygen Documentation"),qa.forEach(r),v.forEach(r),tt.forEach(r),Q.forEach(r),Te=u(F),H=n(F,"DIV",{class:!0});var Et=l(H);i=n(Et,"DIV",{});var c=l(i);st=n(c,"H4",{});var Ua=l(st);Be=a(Ua,"Want to know more?"),Ua.forEach(r),De=a(c,`
+        \xBB
+        `),ht=n(c,"STRONG",{});var ja=l(ht);ke=a(ja,"Looking for tutorials/code snippets?"),ja.forEach(r),Re=a(c,` Check out our
+        `),q=n(c,"A",{href:!0});var Na=l(q);we=a(Na,"HELICS Examples Repository"),Na.forEach(r),xe=a(c,`.
+        `),Pe=n(c,"BR",{}),Ge=a(c,`\xBB
+        `),ct=n(c,"STRONG",{});var za=l(ct);Me=a(za,"Looking for videos?"),za.forEach(r),Ve=a(c,` Check out
+        `),U=n(c,"A",{href:!0});var Ka=l(U);We=a(Ka,"HELICS Tutorials playlist"),Ka.forEach(r),Je=a(c,`
+        section.
+        `),Oe=n(c,"BR",{}),qe=a(c,`\xBB
+        `),dt=n(c,"STRONG",{});var Xa=l(dt);Ue=a(Xa,"Want to ask a question?"),Xa.forEach(r),je=a(c,` Come find us on
+        `),j=n(c,"A",{href:!0});var Ya=l(j);Ne=a(Ya,"gitter and get in touch"),Ya.forEach(r),ze=a(c,`.
+        `),Ke=n(c,"BR",{}),Xe=a(c,`\xBB
+        `),ut=n(c,"STRONG",{});var Fa=l(ut);Ye=a(Fa,"Have a feature request or bug report? Want to make a contribution or provide feedback?"),Fa.forEach(r),Fe=a(c,`
+        All development is open source and publically available, check out the sources over at
+        `),N=n(c,"A",{href:!0});var Qa=l(N);Qe=a(Qa,"our GitHub organization"),Qa.forEach(r),Ze=a(c,"."),c.forEach(r),$e=u(Et),s=n(Et,"DIV",{});var f=l(s);ft=n(f,"H4",{});var Za=l(ft);ta=a(Za,"Additional Information"),Za.forEach(r),ea=a(f,`
+        \xBB
+        `),z=n(f,"A",{href:!0});var $a=l(z);aa=a($a,"Use Conda to install HELICS for Python"),$a.forEach(r),oa=a(f,`.
+        `),na=n(f,"BR",{}),ra=u(f),la=n(f,"BR",{}),ia=a(f,`\xBB
+        `),K=n(f,"A",{href:!0});var to=l(K);sa=a(to,`Check out our Windows installers for C/C++ Shared Libraries with Python, MATLAB, and Java
+          language bindings`),to.forEach(r),ha=a(f,`.
+        `),ca=n(f,"BR",{}),da=u(f),ua=n(f,"BR",{}),fa=a(f,`\xBB
+        `),X=n(f,"A",{href:!0});var eo=l(X);pa=a(eo,`Download precompiled C/C++ Shared Libraries for Windows, Mac, Linux platforms to link in
+          your application`),eo.forEach(r),ga=a(f,`.
+        `),ma=n(f,"BR",{}),va=u(f),Ea=n(f,"BR",{}),Ca=a(f,`\xBB
+        `),Y=n(f,"A",{href:!0});var ao=l(Y);ba=a(ao,"Try HELICS in Julia"),ao.forEach(r),_a=a(f,"."),f.forEach(r),Et.forEach(r),F.forEach(r),gt.forEach(r),this.h()},h(){h(A,"class","flex flex-col"),h(B,"href","https://docs.helics.org/en/latest/user-guide/installation/index.html"),h(D,"href","https://docs.helics.org/en/latest/user-guide/fundamental_topics/helics_terminology.html"),h(k,"href","https://docs.helics.org/en/latest/user-guide/examples/fundamental_examples/fundamental_default.html"),h(R,"href","https://beta.mybinder.org/v2/gh/kdheepak/openmod-2019-helics-tutorial/master?urlpath=lab/tree/notebooks/cosimulation-introduction.ipynb"),h(w,"href","https://docs.helics.org/en/latest/user-guide/co-simulation_overview.html"),h(x,"href","https://docs.helics.org/en/latest/user-guide/fundamental_topics/fundamental_topics_index.html"),h(P,"href","https://docs.helics.org/en/latest/user-guide/fundamental_topics/stages.html"),h(G,"href","https://docs.helics.org/en/latest/user-guide/fundamental_topics/timing_configuration.html"),h(M,"href","https://docs.helics.org/en/latest/references/configuration_options_reference.html"),h(V,"href","https://docs.helics.org/en/latest/references/api-reference/C_API.html"),h(W,"href","https://python.helics.org/api/capi-py/"),h(J,"href","https://gmlc-tdc.github.io/HELICS.jl/latest/api/"),h(O,"href","https://docs.helics.org/en/latest/doxygen/index.html"),h(_,"class","grid grid-cols-3"),h(q,"href","https://github.com/GMLC-TDC/HELICS-Examples"),h(U,"href","https://www.youtube.com/channel/UCPa81c4BVXEYXt2EShTzbcg"),h(j,"href","https://gitter.im/GMLC-TDC/HELICS"),h(N,"href","https://github.com/GMLC-TDC/HELICS"),h(z,"href","https://anaconda.org/gmlc-tdc/helics"),h(K,"href","https://github.com/GMLC-TDC/HELICS/releases/latest"),h(X,"href","https://github.com/GMLC-TDC/HELICSBuilder/releases/latest"),h(Y,"href","https://github.com/GMLC-TDC/HELICS.jl"),h(H,"class","grid grid-cols-2 space-x-6"),h(b,"class","grid"),h(I,"class","prose prose-lg max-w-none")},m(pt,gt){io(pt,I,gt),t(I,b),t(b,A),t(A,et),t(et,Ct),t(A,bt),t(A,at),t(at,_t),t(b,It),t(b,_),t(_,L),t(L,ot),t(ot,Lt),t(L,St),t(L,nt),t(nt,yt),t(L,At),t(L,g),t(g,Ht),t(g,B),t(B,Tt),t(g,Bt),t(g,Dt),t(g,kt),t(g,D),t(D,Rt),t(g,wt),t(g,xt),t(g,Pt),t(g,k),t(k,Gt),t(g,Mt),t(g,Vt),t(g,Wt),t(g,R),t(R,Jt),t(_,Ot),t(_,S),t(S,rt),t(rt,qt),t(S,Ut),t(S,lt),t(lt,jt),t(S,Nt),t(S,m),t(m,zt),t(m,w),t(w,Kt),t(m,Xt),t(m,Yt),t(m,Ft),t(m,x),t(x,Qt),t(m,Zt),t(m,$t),t(m,te),t(m,P),t(P,ee),t(m,ae),t(m,oe),t(m,ne),t(m,G),t(G,re),t(_,le),t(_,y),t(y,it),t(it,ie),t(y,se),t(y,T),t(T,he),t(T,M),t(M,ce),t(T,de),t(y,ue),t(y,p),t(p,fe),t(p,pe),t(p,V),t(V,ge),t(p,me),t(p,ve),t(p,Ee),t(p,W),t(W,Ce),t(p,be),t(p,_e),t(p,Ie),t(p,J),t(J,Le),t(p,Se),t(p,ye),t(p,Ae),t(p,O),t(O,He),t(b,Te),t(b,H),t(H,i),t(i,st),t(st,Be),t(i,De),t(i,ht),t(ht,ke),t(i,Re),t(i,q),t(q,we),t(i,xe),t(i,Pe),t(i,Ge),t(i,ct),t(ct,Me),t(i,Ve),t(i,U),t(U,We),t(i,Je),t(i,Oe),t(i,qe),t(i,dt),t(dt,Ue),t(i,je),t(i,j),t(j,Ne),t(i,ze),t(i,Ke),t(i,Xe),t(i,ut),t(ut,Ye),t(i,Fe),t(i,N),t(N,Qe),t(i,Ze),t(H,$e),t(H,s),t(s,ft),t(ft,ta),t(s,ea),t(s,z),t(z,aa),t(s,oa),t(s,na),t(s,ra),t(s,la),t(s,ia),t(s,K),t(K,sa),t(s,ha),t(s,ca),t(s,da),t(s,ua),t(s,fa),t(s,X),t(X,pa),t(s,ga),t(s,ma),t(s,va),t(s,Ea),t(s,Ca),t(s,Y),t(Y,ba),t(s,_a)},p:Ia,i:Ia,o:Ia,d(pt){pt&&r(I)}}}class co extends no{constructor(I){super();ro(this,I,null,so,lo,{})}}export{co as default};
